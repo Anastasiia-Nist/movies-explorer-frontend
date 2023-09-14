28f39f0d5b02af17b12722c92186af295cea31b7
Anastasia Nistratova <a.nistratova14@gmail.com>
2023-09-14 22:28:23 +0300

    fix: commit localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // URL
 export const URL_BEATFILM_MOVIES = 'https://api.nomoreparties.co';
-// export const URL_BASE = 'https://api.diploma-by-anastasiia.nomoreparties.co';
-export const URL_BASE = 'http://localhost:3000';
+export const URL_BASE = 'https://api.diploma-by-anastasiia.nomoreparties.co';
+// export const URL_BASE = 'http://localhost:3000';
 
 // ENDPOINTS
 export const ENDPOINT_ROOT = '/';
